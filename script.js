7df1abfc15eea12978ce738a7798c54727d575ee
phongEloper <phongkorea389@gmail.com>
2025-12-26 16:39:31 +0700

    Add Programming, Tech Stack tabs to My Skills page

--- script.js
+++ script.js
@@ -24,3 +24,23 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+function openSkillTab(evt, tabName) {
+    const tabContents = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.add("hidden");
+        tabContents[i].classList.remove("block");
+    }
+
+    const tabLinks = document.getElementsByClassName("tab-link");
+    for (let i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove("active-tab");
+        tabLinks[i].classList.add("text-slate-500", "hover:bg-slate-50");
+    }
+
+    document.getElementById(tabName).classList.remove("hidden");
+    document.getElementById(tabName).classList.add("block");
+    
+    evt.currentTarget.classList.add("active-tab");
+    evt.currentTarget.classList.remove("text-slate-500", "hover:bg-slate-50");
+}
